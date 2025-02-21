863efcbaec1015b10e55d87369c968639eb26da7
Kumar Shantanu <shantanumoradabad@gmail.com>
2025-02-21 17:00:09 +0530

    added error handling if docs are not provided

--- rag_over_code/vector_store.py
+++ rag_over_code/vector_store.py
@@ -1,9 +1,15 @@
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+# from langchain_huggingface import HuggingFaceEmbeddings
+
 from langchain_community.vectorstores import Qdrant
 
 def setup_vectorstore(docs: list, embedding_model: str) -> Qdrant:
-    """Initialize embedding model and vector store."""
-    embeddings = HuggingFaceBgeEmbeddings(model_name=embedding_model)
+    """Initialize the embedding model and set up a Qdrant vector store."""
+    embeddings = HuggingFaceBgeEmbeddings(model_name=embedding_model)  # Updated class
+
+    if not docs:
+        raise ValueError("No documents found for vector store indexing!")
+
     qdrant = Qdrant.from_documents(
         docs,
         embeddings,
