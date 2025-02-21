4dba557189756cef947a046ce16f5cf5bcaa454d
Kumar Shantanu <shantanumoradabad@gmail.com>
2025-02-21 16:52:38 +0530

    minor logging additions for tracking inference mode

--- rag_over_code/llm_pipeline.py
+++ rag_over_code/llm_pipeline.py
@@ -2,14 +2,16 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
 from langchain.chains import RetrievalQA
 import torch
-
+import logging
 def setup_llm(model_name: str, max_length: int, temperature: float) -> HuggingFacePipeline:
     """Load a language model and create a text generation pipeline."""
     device = 0 if torch.cuda.is_available() else -1  # Use GPU if available
-
+    if not device:
+        logging.info("Running LLM on GPU")
+    else:
+        logging.warning("Running LLM on  CPU")
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
-
     pipe = pipeline(
         "text-generation",
         model=model,
